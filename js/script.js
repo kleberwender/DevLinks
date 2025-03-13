962584c391856b9e397e287a002e26764230b5f5
kleberwender <wenderjacome@live.com>
2025-03-13 00:21:23 -0300

    add .. script

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ toggleMode = () => {
   // Substituir a imagem
   if(html.classList.contains("light")) {
     //Se tiver light mode, adicionar a imagem light e mudar o alt
-    img.setAttribute("src", "./assets/avatar_kleber.jpg")
+    img.setAttribute("src", "../assets/avatar_kleber.jpg")
     img.setAttribute("alt", "foto para o tema dark de kleber de Brito usando oculos de Grau Palito preto Camisa Branca e gravata azul")
   } else {
     // se tiver sem light mode, adicionar a imagem normal e mudar o alt
-    img.setAttribute("src", "./assets/avatar_kleber.jpg")
+    img.setAttribute("src", "../assets/avatar_kleber.jpg")
     img.setAttribute("alt", "foto para o tema dark de kleber de Brito usando oculos de Grau Palito preto Camisa Branca e gravata azul")
   }
 }
