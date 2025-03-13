2c514268d859da6cc75e78a425527463ba204573
kleberwender <wenderjacome@live.com>
2025-03-13 00:14:03 -0300

    Commit initail all arquivos

---
+++ js/script.js
@@ -0,0 +1,18 @@
+toggleMode = () => {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // Substituir a imagem
+  if(html.classList.contains("light")) {
+    //Se tiver light mode, adicionar a imagem light e mudar o alt
+    img.setAttribute("src", "/assets/avatar_kleber.jpg")
+    img.setAttribute("alt", "foto para o tema dark de kleber de Brito usando oculos de Grau Palito preto Camisa Branca e gravata azul")
+  } else {
+    // se tiver sem light mode, adicionar a imagem normal e mudar o alt
+    img.setAttribute("src", "/assets/avatar_kleber.jpg")
+    img.setAttribute("alt", "foto para o tema dark de kleber de Brito usando oculos de Grau Palito preto Camisa Branca e gravata azul")
+  }
+}
